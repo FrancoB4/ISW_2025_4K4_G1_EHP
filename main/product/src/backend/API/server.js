b1f8f46d98bf71d8d7f7c0c0e499bcb4d4cf978a
LucioMorales <lucio.moralesdemaria@gmail.com>
2025-04-15 11:19:00 -0300

    modificacion para que se creen correctamente los modelos en la bd

--- main/product/src/backend/API/server.js
+++ main/product/src/backend/API/server.js
@@ -1,9 +1,11 @@
 import { db_init } from "./config/db_init.js";
 import app from "./app.js";
 import { sequelize } from "./config/db.js";
-
 import { Activity } from "./models/activity.js";
-const Actividad = new Activity(sequelize);
+import { RegistrationDetail } from "./models/registration_detail.js";
+import { Registration } from "./models/registration.js";
+import { Schedule } from "./models/schedule.js";
+
 
 // Inicia el servidor
 db_init().then(() => {
