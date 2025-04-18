13847b7dc220012796ce8f61051cee6d37f2cec1
BenjaCimatti <benja.cimatti@gmail.com>
2025-04-18 14:56:16 -0300

    request and response body descriptions for important endpoints

--- main/product/src/backend/API/routes/activity.routes.js
+++ main/product/src/backend/API/routes/activity.routes.js
@@ -4,6 +4,24 @@ import { createActivity, getAllActivities, getOne } from '../controllers/activit
 const activityRouter = express.Router();
 
 activityRouter.get('/', getAllActivities);
+// { response body
+//     "Tirolesa":{
+//         "cant_horarios_disponibles": 4, //Si es cero, el front muestra que no hay horarios para esa actividad
+//         "cupo_maximo": 15,
+//         "horarios":[
+//             {
+//                 "id": 3,
+//                 "hora": "09:00",
+//                 "disponible": true,
+//                 "cupos_restantes": 3
+//             },{...}
+//         ]
+//     },
+//     "Safari":{...},
+//     "Palestra":{...},
+//     "Jardineria":{...}
+// }
+
 activityRouter.post('/', createActivity);
 activityRouter.get('/:id', getOne);
 
--- main/product/src/backend/API/routes/registration.routes.js
+++ main/product/src/backend/API/routes/registration.routes.js
@@ -6,6 +6,19 @@ const registrationRouter = express.Router();
 
 registrationRouter.get('/', getAllRegistrations);
 registrationRouter.post('/', createRegistrations);
+// { request body
+//     "id_horario": 3,
+//     "email": "test@gmail.com",
+//     "visitors": [
+//         {
+//             "nombre_completo": "pablo",
+//             "dni": "22222222",
+//             "fecha_nacimiento": "00/00/000",
+//             "talle_ropa": "XL" //Puede ser nulo
+//         }, {...}
+//     ]
+// }
+
 registrationRouter.get('/details', getAllRegDetails);
 
 export default registrationRouter
